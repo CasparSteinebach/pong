5e5dffdab40316d3d14cafcd4683754b90a4e9d2
Caspar Steinebach <casparsteinebach@gmail.com>
2021-02-06 15:09:47 +0100

    field ready :)

--- pong.js
+++ pong.js
@@ -9,3 +9,21 @@ const height = 600;
 canvas.width = width;
 canvas.height = height;
 let context = canvas.getContext('2d');
+
+window.onload = () => {
+    document.body.appendChild(canvas);
+    animate(step);
+}
+
+let step = () => {
+    update();
+    render();
+    animate(step);
+};
+
+let update = () => {};
+
+let render = () => {
+    context.fillStyle = "#FF00FF";
+    context.fillRect(0, 0, width, height);
+};
